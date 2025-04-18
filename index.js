3cea47e99448749ae3162c302dc796cb7774b45d
sunqfuu <sagir.o@outlook.com>
2025-04-18 16:13:19 +0300

    Refactor index.js to enhance string manipulation examples and improve code structure

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+"use strict";
+
 console.log("hello world!")
 
 let number = 5
@@ -5,3 +7,28 @@ console.log(number)
 
 let number2 = 10
 console.log(number2)
+
+//////////////////////////// ES 5 ///////////////////////////
+// String Access
+
+let data1 = "Sunqfu Node JS"
+console.log(data1.charAt(0)); 
+console.log(data1[0]);
+
+
+let multiLine = `Hello
+World
+This
+is
+a
+multiline
+string
+`
+console.log(multiLine)
+
+let multiLineString = "1.Satır\n" + "2.satır\n" + "3.satır\n"
+console.log(multiLineString)
+
+//Date Now 
+let currentTimeMillis = Date.now()
+console.log(currentTimeMillis)
