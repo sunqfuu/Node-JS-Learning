471a03467bb5a807cafbc431b4a71e81ea74633d
sunqfuu <sagir.o@outlook.com>
2025-04-18 16:51:58 +0300

    Refactors ES5 examples into modular functions
    Modularizes string access, multiline strings, date handling,
    and JSON operations into reusable functions for better readability
    and maintainability. Adds a sample object creation function.
    
    Improves code clarity by standardizing syntax with semicolons.

--- index.js
+++ index.js
@@ -1,34 +1,72 @@
 "use strict";
 
-console.log("hello world!")
+console.log("hello world!");
 
-let number = 5
-console.log(number)
+let number = 5;
+console.log(number);
 
-let number2 = 10
-console.log(number2)
+let number2 = 10;
+console.log(number2);
 
 //////////////////////////// ES 5 ///////////////////////////
 // String Access
+let stringAccessFunction = () => {
+  let data1 = "Sunqfu Node JS";
+  console.log(data1.charAt(0));
+  console.log(data1[0]);
+};
 
-let data1 = "Sunqfu Node JS"
-console.log(data1.charAt(0)); 
-console.log(data1[0]);
+// ES 5 Multiline
 
-
-let multiLine = `Hello
+//ES 5 Multiline String
+let multiLineNowFunction = () => {
+  let multiLineString = "1.Satır\n" + "2.satır\n" + "3.satır\n";
+  let multiLine = `Hello
 World
 This
 is
 a
 multiline
 string
-`
-console.log(multiLine)
+`;
+  console.log(multiLine);
+  console.log(multiLineString);
+};
 
-let multiLineString = "1.Satır\n" + "2.satır\n" + "3.satır\n"
-console.log(multiLineString)
+//ES 5 Date Now
+let dateNowFunction = () => {
+  let currentTimeMillis = Date.now();
+  console.log(currentTimeMillis);
+};
 
-//Date Now 
-let currentTimeMillis = Date.now()
-console.log(currentTimeMillis)
+// ES 5 trim()
+let trimFunction = () => {
+    const sunqfuTrim = "   Node JS Learning ";
+    console.log(sunqfuTrim);
+    console.log(sunqfuTrim.length);
+    console.log(sunqfuTrim.trim().length);
+
+};
+trimFunction();
+
+const personObject = function () {
+    let person = {
+        name: "Steven",
+        surname: "Wilson",
+        number: 44,
+        isLogin: true,
+    }
+    return person;
+}
+
+// ES 5 - JSON.stringify() JSON.parse()
+
+let jsonStringifyParseFunction = () => {
+    const person = personObject();
+    let stringifyObject = JSON.stringify(person);
+    console.log(stringifyObject);
+
+    let parseObject = JSON.parse(stringifyObject);
+    console.log(parseObject);
+}
+jsonStringifyParseFunction();
