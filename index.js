dd4fa4ee4893f69bfd37cd05ab61de32e5ff32bd
Orhan Sağır <sagir.o@outlook.com>
2025-04-24 13:31:10 +0300

    Refactor index.js to add ES5 array methods examples and update package-lock.json for nodemon version 3.1.10

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 "use strict";
 
+const { response } = require("express");
+const express = require("express");
+const app = express();
+
 console.log("hello world!");
 
 let number = 5;
@@ -70,3 +74,54 @@ let jsonStringifyParseFunction = () => {
     console.log(parseObject);
 }
 jsonStringifyParseFunction();
+
+
+// ES 5 - isArray ()
+let isArrayFunction = () => {
+  let array = [1, 2, 3, 4, 5];
+  let arrayData = Array.isArray(array);
+  console.log(arrayData);
+  if (arrayData) {  
+    console.log("This is an array.");
+  }else{
+    console.log("This is not an array.");
+  }
+}
+
+isArrayFunction();
+
+// ES 5 - forEach ()
+let forEachFunction = () => {
+  let array = [1, 2, 3, 4, 5];
+  let arrayData = array.forEach((response) => {
+    console.log(response * 2);
+  } )
+
+}
+
+forEachFunction();
+
+// ES 5 - map ()
+
+let mapFunction = () => {
+  let array = [1, 2, 3, 4, 5];
+  let arrayData = array.map((response) => {
+    return response * 2;
+  })
+  console.log(arrayData);
+}
+mapFunction();
+
+// ES 5 - filter ()
+let filterFunction = () => {
+  let array = [1, 2, 3, 4, 5];
+  let arrayData = array.filter((response) => {
+    response % 2 === 0;
+
+  }).forEach((response) => {
+    console.log(response);
+  })
+  console.log(arrayData);
+}
+
+filterFunction();
