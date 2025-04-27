56bb8c6a215a4a2b6cf6904f13208dcc507192df
Orhan Sağır <sagir.o@outlook.com>
2025-04-27 17:15:00 +0300

    Adds dirname and filename demo script
    Creates a new JavaScript file demonstrating the use of Node.js
    built-in __dirname and __filename global variables with basic
    string manipulation examples.

---
+++ _3_file_io/dirname_filename.js
@@ -0,0 +1,12 @@
+//Filename
+console.log(__filename);
+
+const fileNameValue = __filename;
+console.log("File Value: " +fileNameValue);
+console.log("File Value: " +fileNameValue.substring(0,12));
+
+//Directory
+console.log(__dirname);
+const dirNameValue = __dirname;
+console.log("Dir Value: " +dirNameValue);
+console.log("Dir Value: " +dirNameValue.substring(0,12));
