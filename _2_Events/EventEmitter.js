75ae248eb2e3a2fcf75fc8a220dc42c7cf126e40
Orhan Sağır <sagir.o@outlook.com>
2025-04-27 15:20:29 +0300

    Move nodemon to devDependencies and add event emitter
    Relocates nodemon package from dependencies to devDependencies for proper development tooling organization.
    
    Introduces basic event emitter implementation with file creation event listener and periodic trigger.

---
+++ _2_Events/EventEmitter.js
@@ -0,0 +1,25 @@
+// Node JS Event Driven
+// Event Trigger (Olay Tetiklenmesi)
+// Event And Listening
+
+// Event Module Create
+const myEvent = require('events');
+
+// Emitter Class
+const myEmitter = new myEvent.EventEmitter();
+
+// Event Name
+const eventSpecialDataCreate = "fileCreate";
+
+// Event Listener
+myEmitter.on(eventSpecialDataCreate,() => {
+console.log("fileCreate Event Listener");
+});
+
+// Event Trigger
+myEmitter.emit(eventSpecialDataCreate);
+
+//
+setInterval(() => {
+    myEmitter.emit(eventSpecialDataCreate);
+}, 1000)
