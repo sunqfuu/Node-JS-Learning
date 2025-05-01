6580d859b781a100f11c1a7db8df2c3cd016613f
Orhan Sağır <sagir.o@outlook.com>
2025-05-01 10:43:08 +0300

    Enhances routing and adds HTML file serving
    - Implements proper file reading for HTML pages
    - Fixes fs.readFile method calls
    - Reorganizes router configuration
    - Adds 404 error handling with custom page
    - Replaces static responses with dynamic HTML content

--- bolumsonucanavarı/expressRouter.js
+++ bolumsonucanavarı/expressRouter.js
@@ -4,26 +4,27 @@ console.log(__dirname);
 const express = require("express");
 const app = express();
 
+//Express Router
+const router = express.Router();
+
 //Router
 const port = 4444;
 
 //FileSystem module
 const fs = require("fs");
+const path = require("path");
 
 //Pages
 const indexPage = "./index.html";
 const loginPage = "./login.html";
 const registerPage = "./register.html";
-const notFoundPage = "./notfound.html";
-
-//Express Router
-const router = express.Router();
+const notfoundPage = "./notfound.html";
 
 //Homepage
 router.get("/", (request, response) => {
-  response.send("Hellow Knight");
+  // response.send("Hellow Knight");
 
-  fs.readfile(indexPage, (err, data) => {
+  fs.readFile(indexPage, (err, data) => {
     if (err) throw err;
     response.end(data);
   });
@@ -31,12 +32,32 @@ router.get("/", (request, response) => {
 
 //Login
 router.get("/login", (request, response) => {
-  response.send("Login Page");
+  // response.send("Login Page");
+
+  fs.readFile(loginPage, (err, data) => {
+    if (err) throw err;
+    response.end(data);
+  });
 });
 
-//Homepage
+//Register
 router.get("/register", (request, response) => {
-  response.send("Register Page");
+  // response.send("Register Page");
+
+  fs.readFile(registerPage, (err, data) => {
+    if (err) throw err;
+    response.end(data);
+  });
+});
+
+// 404 notfound
+app.use((request, response, next) => {
+  // response.send("404 Page Not Found");
+
+  fs.readFile(notfoundPage, (err, data) => {
+    if (err) throw err;
+    response.end(data);
+  });
 });
 
 //Router
